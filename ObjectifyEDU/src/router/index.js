5c1acf6167c7622146d1a92b337e35d3cd975b12
lochi <22205276@life.hkbu.edu.hk>
2024-03-14 18:17:25 +0800

    backup

--- ObjectifyEDU/src/router/index.js
+++ ObjectifyEDU/src/router/index.js
@@ -1,47 +1,55 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import LoginView from '../views/LoginView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: LoginView // default route to Login Form
+      path: "/",
+      name: "home",
+      component: LoginView, // default route to Login Form
     },
     {
-      path: '/Dashboard',
-      name: 'Dashboard',
+      path: "/Dashboard",
+      name: "Dashboard",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/HomeView.vue')
+      component: () => import("../views/HomeView.vue"),
     },
     {
-      path: '/Courses',
-      name: 'Courses',
+      path: "/Courses",
+      name: "Courses",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/CoursesView.vue')
+      component: () => import("../views/CoursesView.vue"),
     },
     {
-      path: '/Courses/Games/:id',
-      name: 'Games',
+      path: "/Courses/Games/:id",
+      name: "Games",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/GamesView.vue')
+      component: () => import("../views/GamesView.vue"),
     },
     {
-      path: '/Courses/Games/:id/:gameId',
-      name: 'GamesPage',
+      path: "/Courses/Games/:id/:gameId",
+      name: "GamesPage",
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/GamePageView.vue')
+      component: () => import("../views/GamePageView.vue"),
     },
-  ]
-})
+    {
+      path: "/faceEmotion",
+      name: "faceEmotion",
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/faceEmotionView.vue"),
+    },
+  ],
+});
 
-export default router
+export default router;
